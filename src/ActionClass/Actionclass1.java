769cc949c2d3db781558c2f7681d809335e2d4d5
Ashwini <taleashwini98@gmail.com>
2024-10-30 18:29:36 +0530

    [AshwiniTale] Adding my first code

--- src/ActionClass/Actionclass1.java
+++ src/ActionClass/Actionclass1.java
@@ -1,10 +1,17 @@
 package ActionClass;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
 public class Actionclass1 {
 
 	public static void main(String[] args) 
 	{
 		
+		WebDriver driver=new ChromeDriver();
+		
+		Actions act=new Actions(driver);
 		
 
 	}
